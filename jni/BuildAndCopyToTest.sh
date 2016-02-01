b0602f83aba77cdfa56c4741864220d60909a7a1
Iker Jamardo Zugaza <ijamardo@ideateca.com>
2016-01-31 17:15:50 -0800

    Improved the idk building script.

--- jni/BuildAndCopyToTest.sh
+++ jni/BuildAndCopyToTest.sh
@@ -1,10 +1,9 @@
 # Rebuilt (-B) specifying the makefile to use and the output folder for the final libraries and intermediate files
 echo "Rebuilding..."
 $ANDROID_NDK_PATH/ndk-build NDK_APPLICATION_MK=./Application.mk NDK_LIBS_OUT=../build NDK_OUT=./objs -B
-# Copy the final libaries to the default test if there were no errors while rebuilding
 if [ $? -ne 0 ];then exit $?;fi
 echo "Rebuilt!"
-echo "Copying final libraries to the default test..."
+echo "Copying final libraries to the default test and the crosswalk test..."
 mkdir -p ../test/libs/armeabi-v7a
 if [ $? -ne 0 ];then exit $?;fi
 cp ../build/armeabi-v7a/*.so ../test/libs/armeabi-v7a
