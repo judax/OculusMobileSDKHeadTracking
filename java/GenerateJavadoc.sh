1a8f8fe85812c9ac2b4ba91576d2912645cefd31
Iker Jamardo Zugaza <ijamardo@ideateca.com>
2016-02-06 11:10:31 -0800

    Changed the bash scripts for ant builders. A more cross-platform solution ;).

--- java/GenerateJavadoc.sh
+++
@@ -1,5 +0,0 @@
-echo "Generating javadoc..."
-rm -rf ../javadoc
-javadoc -classpath $ANDROID_HOME/platforms/android-9/android.jar -d ../javadoc src/com/judax/oculusmobilesdkheadtracking/*.java
-if [ $? -ne 0 ];then exit $?;fi
-echo "Javadoc generated!"
