bf9a3d2367e6ed4c7974b817dd5ebe5bf06a86e3
Iker Jamardo Zugaza <ijamardo@ideateca.com>
2016-02-22 19:16:31 -0800

    Removed NDKBuild.sh and improved the .so final product copy message and readability of the build phase in Xcode.

--- jni/BuildAndCopyToTest.sh
+++ jni/BuildAndCopyToTest.sh
@@ -3,7 +3,7 @@ echo "Rebuilding..."
 $ANDROID_NDK_PATH/ndk-build NDK_APPLICATION_MK=./Application.mk NDK_LIBS_OUT=../build NDK_OUT=./objs -B
 if [ $? -ne 0 ];then exit $?;fi
 echo "Rebuilt!"
-echo "Copying final libraries to the default test and the crosswalk test..."
+echo "Copying final libraries to the default test..."
 mkdir -p ../test/libs/armeabi-v7a
 if [ $? -ne 0 ];then exit $?;fi
 cp ../build/armeabi-v7a/*.so ../test/libs/armeabi-v7a
